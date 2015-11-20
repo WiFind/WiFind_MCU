caa348ebffc8c4b334508052f1bd28aaa68ed638
Mingzhi <yum8@mcmaster.ca>
2015-11-19 22:18:48 -0500

    Now scan and send the result every 10 seconds.

--- Src/uart.c
+++ Src/uart.c
@@ -11,7 +11,9 @@
 #include "uart.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "portmacro.h"
 #include <string.h>
+#include <stdbool.h>
 
 
 /*
@@ -36,21 +38,26 @@ UART_HandleTypeDef huart1;
 static const uint8_t TEST_COMMAND [] = "Hello World!";
 static const uint8_t ENTER_COMMAND_MODE [] = "$$$";
 static const uint8_t EXIT_COMMAND_MODE [] = "exit\r\n";
-static const uint8_t CONNECT_TO_SERVER [] = "open 192.168.1.128 8000\r\n";
+static const uint8_t CONNECT_TO_SERVER [] = "open 192.241.160.33 8000\r\n";
 static const uint8_t DISCONNECT_FROM_SERVER [] = "close\r\n";
 static const uint8_t SET_DHCP [] = "set ip dhcp 1\r\n";
-static const uint8_t SET_AUTHENTICATION [] = "set wlan auth 4\r\n";
-static const uint8_t SET_SSID [] = "set wlan ssid WiFind\r\n";
+static const uint8_t SET_AUTHENTICATION [] = "set wlan auth 0\r\n";
+static const uint8_t SET_SSID [] = "set wlan ssid MSetup\r\n";
 static const uint8_t SET_PASSPHRASE [] = "set wlan pass WiFind11\r\n";
 static const uint8_t SET_JOIN_MODE [] = "set wlan join 0\r\n";
 static const uint8_t SAVE [] = "save\r\n";
 static const uint8_t REBOOT [] = "reboot\r\n";
-static const uint8_t JOIN_WIFIND [] = "join WiFind\r\n";
+static const uint8_t JOIN_MSETUP [] = "join MSetup\r\n";
 static const uint8_t SLEEP [] = "sleep\r\n";
 static const uint8_t SCAN [] = "scan 10\r\n";
-static uint8_t head[10];
+static const uint8_t GET_MAC [] = "get mac\r\n";
+static const uint8_t CLOSE_SOCKET[] = "close\r\n";
+static const uint8_t LEAVE[] = "leave\r\n";
+static uint8_t MAC_ADDRESS [18];
+static uint8_t head[30];
 static uint8_t buf[10];
 static uint8_t rx_buffer [2000];
+static uint8_t flag;
 // private function protoypes
 static void MX_USART1_UART_Init(void);
 
@@ -59,26 +66,23 @@ xSemaphoreHandle button_sem;
  * priority 1 is emergency, 2 is periodic message, size is the size of scan fingerprint
  * return the size of the char
  */
-int head_pkt(uint8_t priority, int sizeofscan){
-	head[0] = priority;
-	int ret = 1;
+int head_pkt(bool priority, int sizeofscan){
+    memcpy(head, MAC_ADDRESS, 17);
+    head[17] = ' ';
+
+    if (priority) {
+        head[18] = '1';
+    } else {
+        head[18] = '2';
+    }
+		head[19] = ' ';
+	int ret = 20;
 	while (sizeofscan){
 		head[ret] = '0' + sizeofscan%10;
 		sizeofscan = sizeofscan/10;
 		ret++;
 	}
-	head[ret] = '\0';
-	ret++;
 	return ret;
-}
-
-void receive_redudantline() {
-	HAL_StatusTypeDef rx_status;
-	while (1) {
-		rx_status = HAL_UART_Receive(&huart1, (uint8_t *) buf, 1, 2000);
-		if (buf[0] == '\n')
-			break;
-	}
 }
 
 void send_command(const uint8_t * command, size_t size, size_t line_num) {
@@ -87,12 +91,23 @@ void send_command(const uint8_t * command, size_t size, size_t line_num) {
 		HAL_UART_Transmit(&huart1, (uint8_t *) command + i, 1, 20);
 		//rx_status = HAL_UART_Receive(&huart1, (uint8_t *) buf, 1, 20);
 	}
-	//rx_status = HAL_UART_Receive(&huart1, (uint8_t *) buf, 1, 20);
-	for (size_t i = 0; i < line_num; i++) {
-		//receive_redudantline();
-	}
 }
 
+void get_mac_address(void) {
+	__HAL_UART_CLEAR_IT(&huart1, UART_CLEAR_OREF);
+  __HAL_UART_SEND_REQ(&huart1, UART_RXDATA_FLUSH_REQUEST);
+	size_t size = sizeof(GET_MAC) - 1;
+	HAL_StatusTypeDef rx_status;
+	for (size_t i = 0; i < size; i++) {
+		HAL_UART_Transmit(&huart1, (uint8_t *) GET_MAC + i, 1, 20);
+		rx_status = HAL_UART_Receive(&huart1, buf, 1, 20);
+	}
+	rx_status = HAL_UART_Receive(&huart1, buf, 1, 10);
+	for (size_t i = 0; i < 9; i++) {
+		rx_status = HAL_UART_Receive(&huart1, buf, 1, 20);
+	}
+	rx_status = HAL_UART_Receive(&huart1, MAC_ADDRESS, 18, 100);
+}
 
 int send_scan_command(void) {
     int num_of_scan_result = 0;
@@ -155,40 +170,19 @@ void uart_thread(void const *argument) {
 	button_sem = xSemaphoreCreateBinary();
 	HAL_StatusTypeDef rx_status;
 
-	for (;;) {
-		xSemaphoreTake(button_sem, portMAX_DELAY);
-		osDelay(1000);
-		
 		HAL_UART_Transmit(&huart1, (uint8_t *) ENTER_COMMAND_MODE, sizeof(ENTER_COMMAND_MODE) - 1, 1000);
 		__HAL_UART_CLEAR_IT(&huart1, UART_CLEAR_OREF);
     	__HAL_UART_SEND_REQ(&huart1, UART_RXDATA_FLUSH_REQUEST);
 
 		//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_4);
 		osDelay(500);
-		//rx_status = HAL_UART_Receive(&huart1, (uint8_t *) rx_buffer, 5, 500);
-
-		//HAL_UART_Transmit(&huart1, (uint8_t *) SCAN, sizeof(SCAN) - 1, 1000);
-		//osDelay(4000);
-		//HAL_UART_Transmit(&huart1, (uint8_t *) "h", 1, 10);
-		//HAL_UART_Receive(&huart1, rx_buffer, 1, 20);
-
-		//HAL_UART_Transmit(&huart1, (uint8_t *) "hi12", 4, 20);
-		//HAL_UART_Receive(&huart1, rx_buffer + 1, 4, 40);
-		//rx_status = HAL_UART_Receive(&huart1, (uint8_t *) rx_buffer, 5, 10000);
-
-
-		//huart1.State = HAL_UART_STATE_READY;
-
-		//HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_4);
-
-
-		//send_command(SLEEP, sizeof(SLEEP) - 1);
-
+		get_mac_address();
+		osDelay(100);
 		send_command(SET_AUTHENTICATION, sizeof(SET_AUTHENTICATION) - 1, 2);
-		osDelay(300);
-		send_command(SET_SSID, sizeof(SET_SSID) - 1, 2);
-		osDelay(300);
-		send_command(SET_PASSPHRASE, sizeof(SET_PASSPHRASE) - 1, 2);
+	//	osDelay(300);
+//		send_command(SET_SSID, sizeof(SET_SSID) - 1, 2);
+	//	osDelay(300);
+	//	send_command(SET_PASSPHRASE, sizeof(SET_PASSPHRASE) - 1, 2);
 		osDelay(800);
 		send_command(SET_JOIN_MODE, sizeof(SET_JOIN_MODE) - 1, 2);
 		osDelay(300);
@@ -200,7 +194,17 @@ void uart_thread(void const *argument) {
 		osDelay(300);
 		//rx_status = HAL_UART_Receive(&huart1, (uint8_t *) buf, 8, 500);
 		osDelay(100);
-		
+
+	for (;;) {
+		signed portBASE_TYPE semaphore_status;
+    bool b_emergency_button_pushed = false;
+		semaphore_status = xSemaphoreTake(button_sem, (portTickType) 10000);
+        if (semaphore_status == pdTRUE) {
+            b_emergency_button_pushed = true;
+        }
+		osDelay(1000);
+
+
 		osDelay(300);
 		//enter command mode
 		HAL_UART_Transmit(&huart1, (uint8_t *) ENTER_COMMAND_MODE, sizeof(ENTER_COMMAND_MODE) - 1, 1000);
@@ -209,20 +213,29 @@ void uart_thread(void const *argument) {
     __HAL_UART_SEND_REQ(&huart1, UART_RXDATA_FLUSH_REQUEST);
 
 		int sizeofscan = send_scan_command();
-		send_command(JOIN_WIFIND, sizeof(JOIN_WIFIND) - 1, 12);
+		send_command(JOIN_MSETUP, sizeof(JOIN_MSETUP) - 1, 12);
 		//__HAL_UART_CLEAR_IT(&huart1, UART_CLEAR_OREF);
 		//__HAL_UART_SEND_REQ(&huart1, UART_RXDATA_FLUSH_REQUEST);
-		osDelay(15000);
+		osDelay(10000);
 		send_command(CONNECT_TO_SERVER, sizeof(CONNECT_TO_SERVER) - 1, 1);
 		//__HAL_UART_CLEAR_IT(&huart1, UART_CLEAR_OREF);
 	  //__HAL_UART_SEND_REQ(&huart1, UART_RXDATA_FLUSH_REQUEST);
-		osDelay(1000);
-		int sizeofhead = head_pkt(2, sizeofscan);
+		osDelay(5000);
+		int sizeofhead = head_pkt(b_emergency_button_pushed, sizeofscan);
 		HAL_UART_Transmit(&huart1, (uint8_t *) head, sizeofhead, 1000);
 		HAL_UART_Transmit(&huart1, (uint8_t *) rx_buffer, sizeofscan, 1000);
+		osDelay(1000);
+		__HAL_UART_CLEAR_IT(&huart1, UART_CLEAR_OREF);
+    __HAL_UART_SEND_REQ(&huart1, UART_RXDATA_FLUSH_REQUEST);
+		//rx_status = HAL_UART_Receive(&huart1, (uint8_t *) &flag, 1, 2000);
+		osDelay(500);
+		HAL_UART_Transmit(&huart1, (uint8_t *) ENTER_COMMAND_MODE, sizeof(ENTER_COMMAND_MODE) - 1, 1000);
+		osDelay(500);
+		send_command(CLOSE_SOCKET, sizeof(CLOSE_SOCKET) - 1, 1);
+		osDelay(100);
+		send_command(LEAVE, sizeof(LEAVE) - 1, 1);
+		send_command(REBOOT, sizeof(REBOOT) - 1, 0);
 		osDelay(300);
-
-				
 		/*
 		osDelay(300);
 		send_command(ENTER_COMMAND_MODE, sizeof(ENTER_COMMAND_MODE) - 1);
